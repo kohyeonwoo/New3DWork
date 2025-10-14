144a06e6d32d8242a3d92779c30b47cab274ea1a
kohyeonwoo <49010769+kohyeonwoo@users.noreply.github.com>
2025-10-14 13:27:52 +0900

    251014
    툴 에디터 작성 1

---
+++ New3DWork/Assets/Editor/MapMaker.cs
@@ -0,0 +1,123 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.EditorTools;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+public class MapMaker : EditorWindow
+{
+  
+  private Vector2 offset;
+
+  private Vector2 drag;
+
+  private List<List<Node>> nodes;
+
+  private GUIStyle empty;
+
+  [MenuItem("Window/MapMaker")]
+  private static void OpenWindow()
+  {
+    MapMaker window = GetWindow<MapMaker>();
+    window.titleContent = new GUIContent("Map Maker");
+  }
+
+  private void OnEnable()
+  {
+    empty = new GUIStyle();
+
+    Texture2D icon = Resources.Load("IconText/Empty") as Texture2D;
+
+    empty.normal.background = icon;
+
+    SetUpNodes();
+  }
+
+  private void SetUpNodes()
+  {
+    nodes = new List<List<Node>>();
+
+    for(int i =0; i < 20; i++)
+    {
+        nodes.Add(new List<Node>());
+
+        for(int j = 0; j < 10; j++)
+        {
+           nodes[i].Add(new Node(Vector2.zero, 30,30, empty));
+        }
+    }
+  }
+
+  private void OnGUI()
+  {
+    
+    DrawGrid();
+    
+    ProcessGrid(Event.current);
+
+    if(GUI.changed)
+    {
+        Repaint();
+    }
+  }
+  
+  private void ProcessGrid(Event E)
+  {
+    
+    drag = Vector2.zero;
+
+    switch(E.type)
+    {
+        case EventType.MouseDrag :
+
+        if(E.button == 0)
+        {
+            OnMouseDrag(E.delta);
+        }
+
+        break;
+    }
+
+  }
+
+  private void OnMouseDrag(Vector2 Delta)
+  {
+    drag = Delta;
+
+    GUI.changed = true;
+  }
+
+  private void DrawGrid()
+  {
+    
+    int widthDivider = Mathf.CeilToInt(position.width / 20);
+
+    int heightDivider = Mathf.CeilToInt(position.height / 20);
+
+    Handles.BeginGUI();
+
+    Handles.color = new Color(0.5f, 0.5f, 0.5f, 0.2f);
+
+    offset += drag;
+
+    Vector3 newOffset = new Vector3(offset.x % 20, offset.y % 20, 0);
+
+    for(int i =0; i < widthDivider; i++)
+    {
+        Handles.DrawLine(new Vector3(20 *i, -20, 0) + newOffset, new Vector3(20 * i, position.height, 0) + newOffset);
+    }
+
+    for(int i =0; i < heightDivider; i++)
+    {
+         Handles.DrawLine(new Vector3(-20 , 20*i, 0) + newOffset, new Vector3(position.width, 20*i, 0) + newOffset);
+    }
+
+    Handles.color = Color.white;
+    Handles.EndGUI();
+
+  }
+
+
+}
---
+++ New3DWork/Assets/Scripts/Node.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+   
+    private Rect rect;
+
+    private GUIStyle style;
+
+    public Node(Vector2 Position, float Width, float Height, GUIStyle DefaultStyle)
+    {
+        rect = new Rect(Position.x, Position.y, Width, Height);
+
+        style = DefaultStyle;
+    }
+
+    public void Drag(Vector2 Delta)
+    {
+        rect.position += Delta;
+    }
+
+    public void Draw()
+    {
+        GUI.Box(rect, "", style);
+    }
+
+    public void SetStyle(GUIStyle NodeStyle)
+    {
+        style = NodeStyle;
+    }
+
+}
