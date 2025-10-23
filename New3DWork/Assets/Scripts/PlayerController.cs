47e5ccd17c163ae348570bfed35f7d58908ca1ed
kohyeonwoo <49010769+kohyeonwoo@users.noreply.github.com>
2025-10-23 16:00:55 +0900

    251023
    플레이어블 캐릭터 애니메이션 및 움직임 및 조작법 추가, 회전 오류 부분 수정 예정

---
+++ New3DWork/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField]
+    private Rigidbody rigid;
+
+    [SerializeField]
+    private Animator animator;
+
+    [SerializeField]
+    private FixedJoystick joystick;
+
+    [SerializeField]
+    private float moveSpeed;
+
+    public float maxHealth;
+
+    public float currentHealth;
+
+    private void Start()
+    {
+        maxHealth = currentHealth;
+
+        rigid = this.GetComponent<Rigidbody>();
+
+        animator = this.GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        rigid.velocity = new Vector3(joystick.Horizontal * moveSpeed,
+            rigid.velocity.y, joystick.Vertical * moveSpeed);
+
+        if(joystick.Horizontal != 0 || joystick.Vertical != 0)
+        {
+            this.transform.rotation = Quaternion.LookRotation(rigid.velocity);
+
+            animator.SetBool("bMove", true);
+        }
+        else
+        {
+            animator.SetBool("bMove", false);
+        }
+    }
+
+}
